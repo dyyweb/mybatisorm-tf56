7cae8cab3ef9d04ab7a4e2b1eb9bc4335343cc4e
dengyangyang <dengyang163>
2018-04-03 13:49:42 +0800

    新版本

--- common/ResultModel.java
+++ common/ResultModel.java
@@ -8,101 +8,37 @@ import java.util.List;
  */
 public class ResultModel<T> {
 
-    public static final String SUCCESS="success";
-
-    public static final String FAILED="failed";
     /**
      * 接口调用是否成功
-     * ture 成功、false 失败
      */
     private boolean result = true;
 
     /**
-     * 接口调用返回对象模型
+     * 接口返回内容
      */
     private T model;
 
     /**
-     * 接口调用返回对象集合模型
-     */
-    private List<T> modelList;
-
-    /**
-     * 接口调用失败code
+     * 接口失败code
      */
     private String errorCode;
     /**
-     * 接口调用信息
+     * 接口返回信息
      */
     private String message;
 
-    /**
-     * 接口调用异常类
-     */
-    private Exception exception;
 
-    public ResultModel() {}
+    public ResultModel() {
+    }
 
-    public ResultModel(boolean result, String message,String errorCode) {
+    public ResultModel(String message) {
+        this.message = message;
+    }
+
+    public ResultModel(boolean result, String message, String errorCode) {
         this.result = result;
         this.message = message;
         this.errorCode = errorCode;
     }
 
-    public ResultModel(T model) {
-        this.model = model;
-    }
-
-    public ResultModel(List<T> modelList) {
-        this.modelList = modelList;
-    }
-
-
-    public boolean isResult() {
-        return result;
-    }
-
-    public void setResult(boolean result) {
-        this.result = result;
-    }
-
-    public T getModel() {
-        return model;
-    }
-
-    public void setModel(T model) {
-        this.model = model;
-    }
-
-    public List<T> getModelList() {
-        return modelList;
-    }
-
-    public void setModelList(List<T> modelList) {
-        this.modelList = modelList;
-    }
-
-    public String getErrorCode() {
-        return errorCode;
-    }
-
-    public void setErrorCode(String errorCode) {
-        this.errorCode = errorCode;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Exception getException() {
-        return exception;
-    }
-
-    public void setException(Exception exception) {
-        this.exception = exception;
-    }
 }
--- src/main/java/org/dy/common/orm/mysql/mybatis/Mybatisorm.java
+++ src/main/java/org/dy/common/orm/mysql/mybatis/Mybatisorm.java
@@ -43,15 +43,15 @@ public class Mybatisorm {
 		TablesBuilder builder = new TablesBuilder();
 		Map<OutPathKey,String> outPathMap = new HashMap<OutPathKey,String>();
 		builder.setJdbcClass("com.mysql.jdbc.Driver");//驱动
-		builder.setUrl("jdbc:mysql://10.7.29.121:3306/lfspClearing?useUnicode=true&amp;characterEncoding=utf-8");//数据库链接
-		builder.setName("root");//数据库用户名
-		builder.setPwd("wei328e8,wie");//数据库密码
-		TablesBuilder.BASEPACKAGE = "tf56.spider.base";//base包地址
-		builder.setPojoPackage("tf56.spider.clear.model.domain");//pojo包地址
-		builder.setDtoPackage("tf56.spider.clear.model.dto");//pojo包地址
-		builder.setDaoPackage("tf56.spider.clear.mapper");//dao包地址
-		builder.setServicePackage("tf56.spider.clear.api");
-		builder.setServiceImplPackage("tf56.spider.clear.service");
+		builder.setUrl("jdbc:mysql://116.62.191.149:15381/pay_cashier?useUnicode=true&amp;allowMultiQueries=true&amp;characterEncoding=UTF-8");//数据库链接
+		builder.setName("caocao");//数据库用户名
+		builder.setPwd("ONe/UCpxo2ooso4uCinOcF5uzLHV1Z0tfJJhCZ89Ld+d7Us63265J/xF8+4VvD8fIS8yFKHEwOMGGaUNWe3t+A==");//数据库密码
+		TablesBuilder.BASEPACKAGE = "com.dy.base";//base包地址
+		builder.setPojoPackage("com.dy.model.domain");//pojo包地址
+		builder.setDtoPackage("com.dy.model.dto");//pojo包地址
+		builder.setDaoPackage("com.dy.mapper");//dao包地址
+		builder.setServicePackage("com.dy.api");
+		builder.setServiceImplPackage("com.dy.service");
 		File path_file = new File(PROJECT_PATH);
 		if (!path_file.isDirectory()){
 			path_file.mkdir();
